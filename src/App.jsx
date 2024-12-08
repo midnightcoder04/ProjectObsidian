5f2a7c5dd83f204f56ea9cbb49ebccc92b596da8
midnightcoder04 <125803010+midnightcoder04@users.noreply.github.com>
2024-12-08 11:17:09 +0530

    Initialize Project and Add Fonts

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-6xl text-black'>Welcome to Obsidian</h1>
+    </main>
+  )
+}
+
+export default App
